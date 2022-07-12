f444da4aa319761db3696d43bcf84fcd60175fdc
Felipe Sousa <felipe.fps09@hotmail.com>
2022-07-12 12:09:25 -0300

    Inicialização do projeto

---
+++ backend/src/main/java/com/maurocenter/almox/AlmoxApplication.java
@@ -0,0 +1,13 @@
+package com.maurocenter.almox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmoxApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/maurocenter/almox/AlmoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maurocenter.almox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
