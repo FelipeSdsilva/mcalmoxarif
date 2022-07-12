85885213f525c4e5a81d7581906f7cab8c3416c1
Felipe Sousa <felipe.fps09@hotmail.com>
2022-07-12 19:12:33 -0300

    Nova branch

--- backend/src/main/java/com/maurocenter/almox/entites/Product.java
+++ backend/src/main/java/com/maurocenter/almox/entites/Product.java
@@ -3,6 +3,11 @@ package com.maurocenter.almox.entites;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
