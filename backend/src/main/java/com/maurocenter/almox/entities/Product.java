a32243c7adc39530a316c5f40ec81ef871090db9
Felipe Sousa <felipe.fps09@hotmail.com>
2022-12-01 20:24:30 -0300

    Fix auto increment anotation and anotation Id

--- backend/src/main/java/com/maurocenter/almox/entities/Product.java
+++ backend/src/main/java/com/maurocenter/almox/entities/Product.java
@@ -3,6 +3,9 @@ package com.maurocenter.almox.entities;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -19,6 +22,8 @@ import lombok.Setter;
 public class Product implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double price;
