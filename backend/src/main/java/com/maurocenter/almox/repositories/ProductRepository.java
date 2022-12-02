8c50ba1d20f6923f73b4b13642d5dd7d9c204bdf
Felipe Sousa <felipe.fps09@hotmail.com>
2022-12-01 21:37:47 -0300

    repository product anotation

--- backend/src/main/java/com/maurocenter/almox/repositories/ProductRepository.java
+++ backend/src/main/java/com/maurocenter/almox/repositories/ProductRepository.java
@@ -1,5 +1,11 @@
 package com.maurocenter.almox.repositories;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maurocenter.almox.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
